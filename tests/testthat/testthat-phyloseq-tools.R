3a3b9fbcd9653f9223c4c6a9a2375558ee261132
zach powers <zcharlop@rockefeller.edu>
2015-06-23 15:14:31 -0400

    Add a rarefaction function to rarefy phyloseq objects in parallel. Update docs and namespcae and bump version

--- tests/testthat/testthat-phyloseq-tools.R
+++ tests/testthat/testthat-phyloseq-tools.R
@@ -1,5 +1,6 @@
 library("phyloseq.tools")
 library("testthat")
+library("phyloseq")
 
 df <- data.frame( otu1=c('a','b','c','d','e','f','g','h','i','j','k'), 
                   otu2=c('a','a','c','c','e','e','g','e','i','j','i'),
@@ -9,3 +10,14 @@ df <- data.frame( otu1=c('a','b','c','d','e','f','g','h','i','j','k'),
 test_that("make_tree can create a phylogenetic tree from a dataframe", {
   expect_that(make_tree(df), is_a("phylo"))
 })
+
+
+test_that("curve rarefaction works using single or multiple threads", {
+  data('GlobalPatterns')
+  psdata <- GlobalPatterns
+  expect_that(calculate_rarefaction_curves(psdata,c('Observed', 'Shannon'), c(100,100)), is_a("data.frame"))
+  expect_that(calculate_rarefaction_curves(psdata,c('Observed', 'Shannon'), c(100,100), parallel = TRUE), is_a("data.frame"))
+  expect_that(calculate_rarefaction_curves(psdata,c('Observed', 'Shannon'), c(100,100), parallel = TRUE, ncpus=2), is_a("data.frame"))
+})
+
+
